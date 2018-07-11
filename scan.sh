5637050ddaa48439cdd21c0e8444b32935110f03
redfast00 <redfast00@gmail.com>
2018-07-11 17:06:15 +0200

    Made scan script more robust

--- scan.sh
+++ scan.sh
@@ -9,12 +9,14 @@ BT_STATION_MAC=$(hcitool dev | grep "hci0" | cut -f 3)
 
 echo "INFO, station, MAC_bluetooth=${BT_STATION_MAC}, MAC_ethernet=${ETH_MAC}"
 coproc bluetoothctl
-echo 'scan on' >&${COPROC[1]}
-while read -u ${COPROC[0]} LOGLINE
+echo 'scan on' >&"${COPROC[1]}"
+while read -u "${COPROC[0]}" DIRTY_LOGLINE
 do
-    if [[ $LOGLINE = *"RSSI"* ]]; then
-        BATON_MAC=$(echo "$LOGLINE" | cut -d ' ' -f 4)
-        RSSI=$(echo "$LOGLINE" | cut -d ' ' -f 6)
+    if [[ "${DIRTY_LOGLINE}" =~ "RSSI" ]]; then
+        # Remove color codes | remove newlines | remove whitespace at beginnning and end | squeeze spaces
+        LOGLINE=$(echo "${DIRTY_LOGLINE}" | sed 's/\x1B\[[0-9;]*[JKmsu]//g; s/\r/\n/g' | tr -d '\n' | sed "s/^[ \t]*//" | tr -s ' ')
+        BATON_MAC=$(echo "${LOGLINE}" | cut -d ' ' -f 4)
+        RSSI=$(echo "${LOGLINE}" | cut -d ' ' -f 6)
         echo "${BT_STATION_MAC},IGNORE,${BATON_MAC},${RSSI}"
     fi
 done
