65eb0e73dcbbde9799889a1dde679fbd1943bd72
Francis Begyn <francis.begyn@gmail.com>
2018-07-25 01:35:47 +0200

    Full deploy script + networking

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+systemctl stop systemd-networkd && systemctl disable systemd-networkd
+apt-get update -yyq && apt-get upgrade -yyq
+apt-get install -yyq git bluez netcat-traditional
+git clone https://github.com/12urenloop/STROLL.git /root/STROLL
 cp /root/STROLL/scanner.service /etc/systemd/system
+cp /root/STROLL/networking /etc/network/interfaces.d/12uloop
+service networking stop
+service networking start
 systemctl daemon-reload
-systemctl enable scanner
-systemctl restart scanner
+systemctl enable scanner && systemctl restart scanner
