431580bdbbb5467d906e22041f1a95c9505d2c0e
Francis Begyn <francis.begyn@gmail.com>
2018-11-19 20:07:44 +0100

    Set the IP of cable in the deploy script
    Set the DHCP reservation for cable in the environment file.
    Also add some comments to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 rm -rf /root/STROLL
+echo "Disabling systemd networkd ..."
 systemctl stop systemd-networkd && systemctl disable systemd-networkd
+echo "\n Updating system ..."
 apt-get update -yyq && apt-get upgrade -yyq
 apt-get install -yyq git bluez netcat-traditional
+echo "\n Install STROLL ..."
 git clone https://github.com/12urenloop/STROLL.git /root/STROLL
 cp /root/STROLL/scanner.service /etc/systemd/system
 cp /root/STROLL/networking /etc/network/interfaces
+echo "\n Restarting network ..."
 service networking stop
 service networking start
 systemctl daemon-reload
