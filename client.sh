bf220bb465211409924a03983f8c624829d66399
Francis Begyn <francis.begyn@gmail.com>
2019-04-03 19:43:15 +0200

    change it to use openbsd netcat on arch linux ARM

--- client.sh
+++ client.sh
@@ -5,4 +5,4 @@ PORT="$2"
 
 echo "Server $1 on port $2"
 # Use traditional netcat, to close when stdin closes
-./scan.sh | nc.traditional -q 1 "$SERVER" "$PORT"
+./scan.sh | nc -q 1 "$SERVER" "$PORT"
