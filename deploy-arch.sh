4bd52327a2fef8bd22d9e7970d5a050417bbcecf
Francis Begyn <francis.begyn@gmail.com>
2019-04-04 14:41:03 +0200

    update more things and make it a bit better

--- deploy-arch.sh
+++ deploy-arch.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 echo "Clearing the old directory ..."
-rm -rf /root/STROLL
+rm -rf /home/stroll/STROLL
 #echo "Disabling systemd networkd ..."
 #systemctl stop systemd-networkd && systemctl disable systemd-networkd
-pacman -S --noconfirm openbsd-netcat bluez bluez-utils git
+echo "Installing packages ..."
+pacman -S --noconfirm openbsd-netcat bluez bluez-utils git sudo vim
+echo "Creating stroll user ..."
+useradd -m -G wheel stroll
 echo "Install STROLL ..."
-git clone https://github.com/12urenloop/STROLL.git /root/STROLL
-cp /root/STROLL/scanner.service /etc/systemd/system
+git clone https://github.com/12urenloop/STROLL.git /home/stroll/STROLL
+chown -R stroll:stroll /home/stroll/STROLL
+cp /home/stroll/STROLL/scanner.service /etc/systemd/system
 rm -rf /etc/systemd/network/*
-cp /root/STROLL/networkd/* /etc/systemd/network/
+cp /home/stroll/STROLL/networkd/* /etc/systemd/network/
+echo 'Reload systemd ...'
+systemctl daemon-reload
 echo "Restarting network ..."
 systemctl restart systemd-networkd
-systemctl daemon-reload
+echo "Enabling and starting scanner ..."
 systemctl enable scanner && systemctl restart scanner
-systemctl stop haveged
-systemctl disable haveged getty@ttyS0
