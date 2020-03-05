1694cedde68060228ff9b08f8f7a0d3bdd59ef59
Maxime Bloch <me@mcbloch.dev>
2020-03-05 01:06:56 +0100

    Add option to choose your bluetooth controller

--- scan.sh
+++ scan.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 set -o errexit
 
-ETH_MAC=$(cat /sys/class/net/eth0/address)
+ETH_MAC=$(cat /sys/class/net/wlp58s0/address)
 BT_STATION_MAC=$(bluetoothctl list | cut -d " " -f 2)
+
+controller_count=$(echo "$BT_STATION_MAC"  | wc -l)
+if [[ $controller_count -gt 1 ]]; then
+  echo "Multiple bluetooth controllers detected"
+  echo "Select your preferred one to scan on"
+  echo 
+  i=1
+  for controller in $BT_STATION_MAC; do
+    echo "[$i] $controller"
+    (( i+=1 ))
+  done
+
+  read -p '> ' choice
+  if ! [[ $choice =~ ^[0-9]+$ ]]; then
+    echo "[Error] Please provide a number"
+    exit 1
+  fi
+
+  if [[ $choice -lt 1 || $choice -gt $controller_count ]]; then
+    echo "There is no controller with that number, sorry :/"
+    exit 2
+  fi
+
+  echo "$BT_STATION_MAC"
+  bluetooth_controller=$(echo "$BT_STATION_MAC" | sed -n "${choice}p" )
+
+  BT_STATION_MAC="$bluetooth_controller"
+fi
+
+echo "Using bluetooth controller with mac: [$BT_STATION_MAC]"
 
 # Check if bluetooth is plugged in
 [[ -z  "$BT_STATION_MAC" ]] && echo "MSG, no BT, MAC_ethernet=${ETH_MAC}" && sleep 1 && exit
@@ -13,6 +43,11 @@ echo "power on" >&${COPROC[1]}
 sleep 2
 #echo 'Start scanning'
 echo "scan on" >&${COPROC[1]}
+sleep 2
+echo "menu scan" >&${COPROC[1]}
+sleep 2
+echo "transport le" >&${COPROC[1]}
+sleep 2
 
 echo "INFO, station, MAC_bluetooth=${BT_STATION_MAC}, MAC_ethernet=${ETH_MAC}"
 while read -u "${COPROC[0]}" DIRTY_LOGLINE
